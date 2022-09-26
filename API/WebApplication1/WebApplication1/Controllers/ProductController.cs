4d89a45ebffc43592c73cdb3661f220cc76ba908
TharinduShantha98 <tharindushantha4@gmail.com>
2022-09-26 15:21:49 +0530

    updated

---
+++ API/WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ProductController(IConfiguration configuration) { 
+        
+                _configuration = configuration;
+
+        
+        }
+
+
+
+        [HttpGet]
+        public JsonResult get() {
+
+            string query = @"
+                        select * from Products
+
+            ";
+
+
+            DataTable table = new DataTable();
+            string sqlDataSourse = _configuration.GetConnectionString("ProductAppCon");
+            MySqlDataReader myRender;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSourse)) { 
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon)) { 
+                    
+                    myRender = myCommand.ExecuteReader();
+                    table.Load(myRender);
+
+                    myRender.Close();
+                    mycon.Close();
+
+                
+      
+                }
+            
+            
+            }
+
+            return new JsonResult(table);
+
+
+        }
+
+
+
+    }
+}
---
+++ API/WebApplication1/WebApplication1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+
+        public int ProductId { get; set; }
+        public string Product_name { get; set;}
+        public string Product_description { get; set;}
+
+
+
+
+
+      
+
+
+
+
+    }
+
+}
